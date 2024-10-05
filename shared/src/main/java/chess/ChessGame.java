9bd824f1cbb0ad222fceee249d5b488630c87275
patricktren <patricktrenwarren@gmail.com>
2024-10-04 22:33:26 -0600

    Successfully implemented ChessBoard.getBoard()

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -173,6 +173,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
     }
 }
