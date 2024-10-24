8ed459784e282dc06f939cf5f7183a50118e026f
patricktren <patricktrenwarren@gmail.com>
2024-10-24 13:35:57 -0600

    Refactored the database initialization in the Server class so that it can be assigned as either a MemoryDatabase or a SQLDatabase

--- server/src/main/java/handlers/RegisterHandler.java
+++ server/src/main/java/handlers/RegisterHandler.java
@@ -1,6 +1,7 @@
 package handlers;
 
 import com.google.gson.Gson;
+import dataaccess.Database;
 import dataaccess.MemoryDatabase;
 import exception.ResponseException;
 import model.User;
@@ -10,16 +11,16 @@ import spark.Request;
 import spark.Response;
 
 public class RegisterHandler {
-    protected MemoryDatabase memoryDatabase;
+    protected Database database;
 
-    public RegisterHandler(MemoryDatabase memoryDatabase) {
-        this.memoryDatabase = memoryDatabase;
+    public RegisterHandler(Database database) {
+        this.database = database;
     }
     public Object registerUser(Request req, Response res) throws ResponseException {
         try {
             var newUser = new Gson().fromJson(req.body(), User.class);
             var registerRequest = new RegisterRequest(newUser.getUsername(), newUser.password(), newUser.email());
-            var registerResult = new RegisterService(memoryDatabase).registerUser(registerRequest);
+            var registerResult = new RegisterService(database).registerUser(registerRequest);
             return new Gson().toJson(registerResult);
         } catch (ResponseException er) {
             res.status(er.getStatusCode());
